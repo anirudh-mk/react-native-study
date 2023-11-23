122d252957994333aa415f4898868324d7f2cd03
anirudh-mk <its.me.anirudh.mk@gmail.com>
2023-11-23 22:14:59 +0530

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableWithoutFeedback, Button , Alert, Platform} from 'react-native';
 
 export default function App() {
+  const handleClick = () => console.log('hello')
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeArea style={styles.container}>
+      <TouchableWithoutFeedback 
+        onPress ={
+          ()=>Alert.prompt(
+            'image',
+            'do yo like the image',
+            // (text) => console.log(text)
+          )}>
+        
+        <Image source={require('./assets/favicon.png')}/>
+      </TouchableWithoutFeedback>
+      
       <StatusBar style="auto" />
-    </View>
+      <Button 
+        title='press' 
+        color="red"
+        onPress={()=>Alert.alert(
+          'button',
+          'button clicked',
+          [
+            {
+              text:'yes', 
+              onPress: ()=> console.log('yes')
+            },
+            {
+              text: 'no',
+              onPress: ()=> console.log('no')
+            }
+          ]
+        )}
+      />
+    </SafeArea>
+    
   );
 }
 
@@ -16,5 +46,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: Platform.OS === "android"? StatusBar.currentHeight :20
   },
 });
